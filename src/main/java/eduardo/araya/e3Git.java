b698c9b9489452c36f3f48d2191a57cf5c1072bc
Eduardo Araya <eduardo.araya@tsoftlatam.com>
2020-09-30 11:57:52 -0300

    Revert "commit eliminar tercera vez e3 del repo"
    This reverts commit 506ae484298b625fd8bb6fd5d17b088b4f71e323.

---
+++ src/main/java/eduardo/araya/e3Git.java
@@ -0,0 +1,49 @@
+package eduardo.araya;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class e3Git {
+
+    /*
+    ORDENAR CADENA
+     Cada palabra de la cadena contendrá un solo número
+     Este número es la posición que debería tener la palabra en el resultado
+     Nota: Los números pueden ser del 1 al 9. Por lo tanto, 1 será la primera palabra (no 0).
+     Si la cadena de entrada está vacía, devuelve una cadena vacía.
+     Las palabras en la cadena de entrada solo contendrán números consecutivos válidos.
+
+     Ejemplo:
+       e2s ho1y mierc3oles => ho1y 2es 3miercoles
+     */
+
+    public String ordenarCadena(String s1) {
+
+        //1. dividir String en un Lista de String
+        List<String> listadoPalabras = Arrays.asList(s1.split(" "));
+        List<String> ordenCadena = new ArrayList<String>();
+        int buscadornumero = 1;
+        String delim =" ";
+
+        //recorrer Lista buscando los numeros y ordenandolos
+        do {
+            for (int i = 0; i < listadoPalabras.size(); i++) {
+                if(listadoPalabras.get(i).contains(""+buscadornumero)){
+                    ordenCadena.add(buscadornumero-1,listadoPalabras.get(i));
+                    buscadornumero++;
+                }
+            }
+        }while(listadoPalabras.size() != ordenCadena.size());
+
+        //System.out.println(ordenCadena);
+
+        //convertir la Lista ordenCadena en un String con espacios
+        String res = String.join(delim,ordenCadena);
+        return res;
+    }
+
+
+
+
+}
---
+++ src/test/java/e3GitTest.java
@@ -0,0 +1,17 @@
+import eduardo.araya.e3Git;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class e3GitTest {
+
+    @Test
+    public void ordenarString2palabras(){
+        e3Git ob1 = new e3Git();
+        String s1= "2tu ho1la";
+        assertEquals("ho1la 2tu", ob1.ordenarCadena(s1));
+        System.out.println("String Entrada: "+s1+" .String de Salida: "+ob1.ordenarCadena(s1));
+
+    }
+
+
+}
