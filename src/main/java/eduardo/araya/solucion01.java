f6612a03ef78d85c41773a25de9e358ebcebe2c7
Eduardo Araya <eduardo.araya@tsoftlatam.com>
2020-09-29 22:35:42 -0300

    agregados archivos ejercicio 1 bootcamp

--- src/main/java/eduardo/araya/solucion01.java
+++ src/main/java/eduardo/araya/solucion01.java
@@ -2,15 +2,12 @@ package eduardo.araya;
 
 public class solucion01 {
 
-    public String declararGanador(fighter pj1,fighter pj2){
+
+    public static String declararGanador(fighter pj1, fighter pj2){
 
         double valor = Math.floor(Math.random()*(100-10+1)+10); // Valor entre M y N, ambos incluidos.
 
-        pj1 = new fighter("edu",1000, 10);
-        pj2 = new fighter("ude",1000,100);
-
-
-        return "ganador de la batalla es: "+pj1.name;
+        return pj1.name;
     }
 
 
--- src/test/java/solucion01Test.java
+++ src/test/java/solucion01Test.java
@@ -1,10 +1,13 @@
-import org.junit.Assert;
+import eduardo.araya.fighter;
+import static org.junit.Assert.*;
+import eduardo.araya.solucion01;
 import org.junit.Test;
 
 public class solucion01Test {
 
     @Test
     public void Test(){
-        Assert.fail("no implementado aún");
+        assertEquals("edu", solucion01.declararGanador(new fighter("edu", 10, 2),new fighter("frank", 5, 4)));
+
     }
 }
