b6c6b9c7e0701a21c1885f41e3a14c2e5817b6ad
Eduardo Araya <eduardo.araya@tsoftlatam.com>
2020-10-01 11:26:23 -0300

    se agrega nuevo unit test

--- src/test/java/e3GitTest.java
+++ src/test/java/e3GitTest.java
@@ -5,13 +5,21 @@ import org.junit.Test;
 public class e3GitTest {
 
     @Test
-    public void ordenarString2palabras(){
+    public void ordenarString2palabras() {
         e3Git ob1 = new e3Git();
-        String s1= "2tu ho1la";
+        String s1 = "2tu ho1la";
         assertEquals("ho1la 2tu", ob1.ordenarCadena(s1));
-        System.out.println("String Entrada: "+s1+" .String de Salida: "+ob1.ordenarCadena(s1));
+        System.out.println("String Entrada: " + s1 + " .String de Salida: " + ob1.ordenarCadena(s1));
 
     }
 
+    @Test
+    public void ordenarString4palabras() {
+        e3Git ob1 = new e3Git();
+        String s1 = "2tu ho1la";
+        assertEquals("ho1la 2tu", ob1.ordenarCadena(s1));
+        System.out.println("String Entrada: " + s1 + " .String de Salida: " + ob1.ordenarCadena(s1));
 
+
+    }
 }
