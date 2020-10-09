0611b75269a06ceec554d0820b11cad379551aa0
Eduardo Araya <eduardo.araya@tsoftlatam.com>
2020-10-09 09:52:20 -0300

    commit para agregar mi package

---
+++ src/main/java/jose/juan/solucion01.java
@@ -0,0 +1,14 @@
+package jose.juan;
+
+public class solucion01 {
+
+
+    public static String declararGanador(fighter pj1, fighter pj2){
+
+        double valor = Math.floor(Math.random()*(100-10+1)+10); // Valor entre M y N, ambos incluidos.
+
+        return pj1.name;
+    }
+
+
+}
